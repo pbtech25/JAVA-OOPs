0db040cda4fc58c38582675cf6be8bdba84a39e2
pbtech25 <pbtech25@gmail.com>
2024-06-03 23:20:39 +0530

    learnt Java Constructors with Parameters

---
+++ constWithParam.java
@@ -0,0 +1,10 @@
+public class constWithParam {
+    int modelYear;
+    String model, carName; 
+
+    public constWithParam(String car_name, String model_name, int year){
+        carName = car_name;
+        model = model_name;
+        modelYear = year;
+    }
+}
