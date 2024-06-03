4bb7824b49b74711840c951b783e501e5a49b1cc
pbtech25 <pbtech25@gmail.com>
2024-06-03 22:23:16 +0530

    Learnt basics of Java OOPs

--- Main.java
+++ Main.java
@@ -1,31 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        // OOP myObj1 = new OOP(); //creating object of OOP class
-        // OOP myObj2 = new OOP(); //creating object of OOP class
-        // OOP myObj3 = new OOP(); //creating object of OOP class
-        //
-        // myObj1.x = 40;
-        // myObj2.x = 30;
-        //
-        // System.out.println(myObj1.x + 1);
-        // System.out.println(myObj2.x+2);
-        // System.out.println(myObj3.x+3);
-        //
-        // OOP myName = new OOP(); //creating object of OOP class
-        //
-        // System.out.println("Name: " + myName.fName + " " + myName.lName +
-        // "\nAge: " + myName.age );
-        //
-        // OOP.myStaticMethod();
-        //
-        // OOP public_obj = new OOP();
-        // public_obj.myPublicMethod();
-        //
-        // OOP myCar = new OOP(); //created object myCar
-        // myCar.fullThrottle(); //called fullThrottle method
-        // myCar.speed(200); //called speed method
+        OOP myObj1 = new OOP();
+        System.out.println(myObj1.x + 1);
 
-        OOP myObj = new OOP("Range Rover", "Velar", 2024); // created object of class OOP
-        System.out.println("Car name: " + myObj.name + "\nCar model: " + myObj.model + "\nPurchase Year: " + myObj.year);
+        // modifying attributes
+        myObj1.x = 40;
+        System.out.println(myObj1.x);
+
+        // myObj1.y = 20; //cannot override final int y
+        /*
+         * The final keyword is useful when you want a variable to always store the same
+         * value, like PI (3.14159...).
+         */
+
+        // MULTIPLE OBJECTS
+        OOP myObj2 = new OOP();
+        myObj2.x = 25; // can modify indi. obj. w/o affecting other object
+        System.out.println("Multiple objects: " + myObj1.x); // can modify indi. obj. w/o affecting other
+        System.out.println("Multiple objects: " + myObj2.x);
+
+        // MULTIPLE ATTRIBUTES
+        OOP firstName = new OOP();
+        OOP lastName = new OOP();
+        OOP myAge = new OOP();
+        System.out.println("(Using multiple attributes) \nMy Name is " + firstName.fName + " " + lastName.lName + ". " + "I am " + myAge.age + " years old.");
+
+        
+
     }
 }
